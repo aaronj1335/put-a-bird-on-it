6fa92842a5311e024d54c0b9085c8e6049156ba8
Aaron Stacy <aaron.r.stacy@gmail.com>
2012-07-23 11:32:34 -0500

    got unit tests running

--- src/bird.js
+++ src/bird.js
@@ -1,9 +1,9 @@
 define([
-    'jquery/jquery',
+    'jquery',
     'css!./bird.css'
 ], function($) {
     return function(el) {
-        $('<div').addClass('with-a-bird-on-it').appendTo(el);
+        $('<div>').addClass('with-a-bird-on-it').appendTo(el);
         return el;
     };
 });
